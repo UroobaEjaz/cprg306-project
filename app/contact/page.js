3b07c3082a75a55b392960f7e891a473e60741e6
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-04-16 11:27:18 -0600

    removing apos

--- app/contact/page.js
+++ app/contact/page.js
@@ -9,7 +9,7 @@ const ContactPage = () => {
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold mb-4">Contact Us</h1>
         <p className="text-lg mb-4">
-          Have a question or feedback? We &apos;'d love to hear from you!
+          Have a question or feedback? We would love to hear from you!
         </p>
         <form className="max-w-md">
           <div className="mb-4">
